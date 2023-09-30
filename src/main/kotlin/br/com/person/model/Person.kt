d446d51ce11ff6f2c34ab6058cbc74df3632caef
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-30 13:46:54 -0300

    feat: populate Person Model

--- src/main/kotlin/br/com/person/model/Person.kt
+++ src/main/kotlin/br/com/person/model/Person.kt
@@ -1,5 +1,9 @@
 package br.com.person.model
 
 data class Person(
-    var id: Long = 0
+    var id: Long = 0,
+    var firstName: String=  "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = "",
 )

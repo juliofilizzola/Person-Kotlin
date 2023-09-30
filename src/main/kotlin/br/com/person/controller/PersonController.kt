02796af1791a9a9c06c6995051c3c87417b7e2a1
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-30 15:53:32 -0300

    feat: modify controller person

--- src/main/kotlin/br/com/person/controller/PersonController.kt
+++ src/main/kotlin/br/com/person/controller/PersonController.kt
@@ -1,21 +1,28 @@
 package br.com.person.controller
 
+import br.com.person.convert.ConvertNumber
+import br.com.person.exceptions.UnsupportedMathOperationException
+import br.com.person.model.Person
 import br.com.person.service.PersonService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
+@RequestMapping("/person")
 class PersonController {
     @Autowired
     private lateinit var personService: PersonService
 
-    @RequestMapping(value = ["/person/{id}"])
-    fun findOne(@PathVariable("id") id: String) {
-//        return personService.findOne()
+    @GetMapping(value = ["/{id}"])
+    fun findOne(@PathVariable("id") id: String): Person {
+        if (!ConvertNumber.isNumeric(id)) {
+            throw UnsupportedMathOperationException("Please set a number")
+        }
+        return personService.findOne(ConvertNumber.convertToLong(id))
     }
-
 
 }

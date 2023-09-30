69f9306bd4cf2def2645d8bbb7752674fd6019a3
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-30 15:52:03 -0300

    feat: create customized response error

---
+++ src/main/kotlin/br/com/person/exceptions/CustomizedResponseEntityExceptionHandler.kt
@@ -0,0 +1,35 @@
+package br.com.person.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import java.util.Date
+
+@ControllerAdvice
+@RestController
+class CustomizedResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception::class)
+    fun handlerAllExceptions(ex: Exception, request: WebRequest):
+            ResponseEntity<ExceptionsResponse> {
+        val exceptionsResponse = ExceptionsResponse(
+            Date(),
+            ex.message,
+            request.getDescription(false)
+        )
+        return ResponseEntity<ExceptionsResponse>(exceptionsResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException::class)
+    fun handlerBadRequestExceptions(ex: Exception, request: WebRequest):
+            ResponseEntity<ExceptionsResponse> {
+        val exceptionsResponse = ExceptionsResponse(
+            Date(),
+            ex.message,
+            request.getDescription(false)
+        )
+        return ResponseEntity<ExceptionsResponse>(exceptionsResponse, HttpStatus.BAD_REQUEST)
+    }
+}

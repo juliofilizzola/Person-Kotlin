6abd395712f611860440b34a161ff40bed949e12
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-30 14:05:49 -0300

    feat: create first request

--- src/main/kotlin/br/com/person/controller/PersonController.kt
+++ src/main/kotlin/br/com/person/controller/PersonController.kt
@@ -1,8 +1,21 @@
 package br.com.person.controller
 
+import br.com.person.service.PersonService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class PersonController {
+    @Autowired
+    private lateinit var personService: PersonService
+
+    @RequestMapping(value = ["/person/{id}"])
+    fun findOne(@PathVariable("id") id: String) {
+//        return personService.findOne()
+    }
+
+
 }

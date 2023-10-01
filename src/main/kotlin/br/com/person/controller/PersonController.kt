69fa2d48642fbf2ea399d6389e1ff6125f72410a
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-01 14:21:25 -0300

    feat: create router `create person `

--- src/main/kotlin/br/com/person/controller/PersonController.kt
+++ src/main/kotlin/br/com/person/controller/PersonController.kt
@@ -1,11 +1,14 @@
 package br.com.person.controller
 
 import br.com.person.convert.ConvertNumber
+import br.com.person.dto.PersonDto
 import br.com.person.model.Person
 import br.com.person.service.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -27,5 +30,10 @@ class PersonController {
     @GetMapping
     fun findAll(): List<Person> {
         return personService.findAll()
+    }
+
+    @PostMapping
+    fun create(@RequestBody personDto: PersonDto): Person {
+        return personService.create(personDto)
     }
 }

b0bd523ea5dd09e89af4107e037c55f8a4eb5e4b
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-01 14:30:45 -0300

    feat: create router `delete person`

--- src/main/kotlin/br/com/person/controller/PersonController.kt
+++ src/main/kotlin/br/com/person/controller/PersonController.kt
@@ -5,6 +5,7 @@ import br.com.person.dto.PersonDto
 import br.com.person.model.Person
 import br.com.person.service.PersonService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -35,5 +36,10 @@ class PersonController {
     @PostMapping
     fun create(@RequestBody personDto: PersonDto): Person {
         return personService.create(personDto)
+    }
+
+    @DeleteMapping(value = ["/{id}"])
+    fun delete(@PathVariable("id") id: Long): String {
+        return personService.delete(id)
     }
 }

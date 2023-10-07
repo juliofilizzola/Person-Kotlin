0553fcadabd91f701f9442a9cf717d941105d2b0
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-07 14:40:15 -0300

    feat: create class vo

---
+++ src/main/kotlin/br/com/person/data/vo/v1/PersonVO.kt
@@ -0,0 +1,8 @@
+package br.com.person.data.vo.v1
+
+data class PersonVO(
+    var firstName: String=  "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = "",
+)

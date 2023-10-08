e997c71533ba48690edad12453a79ed3b09344cb
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-08 20:10:58 -0300

    feat: mock person entity and person vo

--- src/test/kotlin/br/com/person/unittest/mapper/mock/MockPerson.kt
+++ src/test/kotlin/br/com/person/unittest/mapper/mock/MockPerson.kt
@@ -1,11 +1,34 @@
 package br.com.person.unittest.mapper.mock
 
+import br.com.person.data.vo.v1.PersonVO
 import br.com.person.model.Person
 import br.com.person.utils.mock.PersonUtils
+import java.util.ArrayList
 
 class MockPerson {
     private val personUtils: PersonUtils = PersonUtils()
     fun mockEntity(): Person {
         return personUtils.mockPerson(0)
     }
+
+   fun mockVO(): PersonVO {
+        return personUtils.mockVO(0)
+    }
+
+    fun mockEntityList(): ArrayList<Person> {
+        val persons: ArrayList<Person> = ArrayList<Person>()
+        for (i in 0..13) {
+            persons.add(personUtils.mockPerson(i))
+        }
+        return persons
+    }
+
+    fun mockVOList(): ArrayList<PersonVO> {
+        val persons: ArrayList<PersonVO> = ArrayList()
+        for (i in 0..13) {
+            persons.add(personUtils.mockVO(i))
+        }
+        return persons
+    }
+
 }

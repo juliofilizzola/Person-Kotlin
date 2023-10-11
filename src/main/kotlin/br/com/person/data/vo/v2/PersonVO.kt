105993e3107d6435475bce28042a658eaa78e62c
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-11 18:55:30 -0300

    feat: modify person vo v2

--- src/main/kotlin/br/com/person/data/vo/v2/PersonVO.kt
+++ src/main/kotlin/br/com/person/data/vo/v2/PersonVO.kt
@@ -1,9 +1,11 @@
 package br.com.person.data.vo.v2
 
+import java.util.Date
+
 data class PersonVO(
     var firstName: String = "",
     var lastName: String = "",
     var address: String = "",
     var gender: String = "",
-    var birthDay: String = ""
+    var birthDay: Date? = null
 )

b726ab23c107726a242e59fc4ac68c73dc7d0234
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-11 15:48:21 -0300

    feat: create person v2

--- src/main/kotlin/br/com/person/controller/PersonController.kt
+++ src/main/kotlin/br/com/person/controller/PersonController.kt
@@ -2,6 +2,7 @@ package br.com.person.controller
 
 import br.com.person.convert.ConvertNumber
 import br.com.person.data.vo.v1.PersonVO
+import br.com.person.data.vo.v2.PersonVO as PersonV02
 import br.com.person.dto.PersonDto
 import br.com.person.service.PersonService
 import org.springframework.beans.factory.annotation.Autowired
@@ -33,6 +34,13 @@ class PersonController {
     fun create(@RequestBody personDto: PersonDto): PersonVO {
         return personService.create(personDto)
     }
+
+    @PostMapping("/v2")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createV2(@RequestBody personDto: PersonV02): PersonV02 {
+        return personService.createV2(personDto)
+    }
+
 
     @PutMapping(value = ["/{id}"])
     fun update(

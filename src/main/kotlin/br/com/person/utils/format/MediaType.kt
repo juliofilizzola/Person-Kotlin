83ec44f8fbca61d9d74a9321cfc37fe8ec244fa3
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-13 17:39:34 -0300

    feat: create MediaType

---
+++ src/main/kotlin/br/com/person/utils/format/MediaType.kt
@@ -0,0 +1,7 @@
+package br.com.person.utils.format
+
+object MediaType {
+    const val APPLICATION_JSON = "application/json"
+    const val APPLICATION_XML = "application/xm l"
+    const val APPLICATION_YML = "application/x-"
+}

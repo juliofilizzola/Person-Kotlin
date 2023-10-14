a7055da84bc3bf56c8897950e2c5f4a0706082b4
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-10-14 14:39:52 -0300

    feat: modify yaml type

--- src/main/kotlin/br/com/person/utils/format/MediaType.kt
+++ src/main/kotlin/br/com/person/utils/format/MediaType.kt
@@ -3,5 +3,5 @@ package br.com.person.utils.format
 object MediaType {
     const val APPLICATION_JSON = "application/json"
     const val APPLICATION_XML = "application/xml"
-    const val APPLICATION_YML = "application/x-yml"
+    const val APPLICATION_YML = "application/x-yaml"
 }
